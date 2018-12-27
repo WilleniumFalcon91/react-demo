aeca36a0b64f551351d9e1b94746a706ec296310
William Elliott <wcelliott1991@gmail.com>
2018-12-27 16:43:57 -0500

    intial project setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import NavBar from "./Navbar";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>More updates to come</p>
           <a
             className="App-link"
             href="https://reactjs.org"
